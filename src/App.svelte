563f7669614b5000e18e9a4a30a33a643dfcf1f6
codacy20 <kmuc72@gmail.com>
2019-10-31 09:19:23 +0100

    simple btn

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,20 @@
 <script>
-	export let name;
+  export let name;
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
 <h1>Hello {name}!</h1>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
